df784e381044525ee50f429d6fe9552bb6cccbef
Matthew Buchanan <mwbuchan92@gmail.com>
2018-07-13 21:00:53 -0400

    initial commits for MVC, server, and client-side React app

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.use(routes);
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nytreact');
+
+app.listen(port, function() {
+  console.log(`🚀 ==> API server listening on port ${PORT}`);
+});
+
